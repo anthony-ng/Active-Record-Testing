82a0ec9ff0d5bec23b8b3abd05ae1b51e3940852
anthony-ng <thomsonng123@gmail.com>
2015-01-18 04:14:53 -0800

    updated models & migration

--- db/migrate/20150118040553_create_ratings.rb
+++ db/migrate/20150118040553_create_ratings.rb
@@ -1,4 +1,10 @@
 class CreateRatings < ActiveRecord::Migration
   def change
+    create_table :ratings do |t|
+      t.belongs_to :user
+      t.belongs_to :skill
+
+      t.timestamps
+    end
   end
 end

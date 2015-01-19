2c55abfb6e8c25a0c1381e303e4bd9c3a7259c55
anthony-ng <thomsonng123@gmail.com>
2015-01-19 03:20:36 -0800

    Edited proficiency_for model in the User table

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,12 +3,4 @@ class Skill < ActiveRecord::Base
   has_many :users, :through => :ratings
 
   validates :name, uniqueness: true
-
-  def self.proficiency_for(skill)
-    #take a skill row for input
-    #search for the corresponding rating
-    #set Rating.proficiency = 0
-    #return Rating.proficiency
-
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_many :ratings
   has_many :skills, :through => :ratings
+
+  def self.proficiency_for(skill)
+    #take a skill row for input
+    #search for the corresponding rating
+    #set Rating.proficiency = 0
+    #return Rating.proficiency
+    Rating.find_by_skill_id(skill.id).update_attributes(proficiency: 0)
+  end
 end

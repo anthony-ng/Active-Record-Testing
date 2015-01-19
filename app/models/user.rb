91dcb605e317818a06d9a12e5c342838e44b18f6
anthony-ng <thomsonng123@gmail.com>
2015-01-19 03:07:43 -0800

    Added pseudocode for solving proficiency_for method problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ActiveRecord::Base
   has_many :ratings
   has_many :skills, :through => :ratings
-
-  def self.proficiency_for(skill)
-    skills = self.find_by(skills: skill)
-    skills.update_attributes(proficiency: 0)
-  end
 end

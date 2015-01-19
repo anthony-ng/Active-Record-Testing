38ec21599ad3d46eadea6622c3a8111d7ce29bad
anthony-ng <thomsonng123@gmail.com>
2015-01-19 02:51:39 -0800

    updated user model method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :skills, :through => :ratings
 
   def self.proficiency_for(skill)
-    skills = self.find_by(skill: skill)
+    skills = self.find_by(skills: skill)
     skills.update_attributes(proficiency: 0)
   end
 end

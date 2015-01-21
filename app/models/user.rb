d81ef9111749bf798760b7b324c6f0c100dea02a
anthony-ng <thomsonng123@gmail.com>
2015-01-20 22:24:44 -0800

    Fixed the 5th test by added default value to proficiency..

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ActiveRecord::Base
   has_many :skills, :through => :ratings
 
   def proficiency_for(skill)
-    self.skills.include?(skill)
+    self.ratings.find_by(skill_id: skill.id).proficiency
   end
 
-  def set_proficiency_for(skill, rank=0)
+  def set_proficiency_for(skill, rank = 0)
     id = self.id
     rating_user = Rating.where(user_id: id)
     rating_user.first.proficiency = rank
--- db/migrate/20150118040553_create_ratings.rb
+++ db/migrate/20150118040553_create_ratings.rb
@@ -3,7 +3,7 @@ class CreateRatings < ActiveRecord::Migration
     create_table :ratings do |t|
       t.belongs_to :user
       t.belongs_to :skill
-      t.integer :proficiency
+      t.integer :proficiency, default: 0
 
       t.timestamps
     end

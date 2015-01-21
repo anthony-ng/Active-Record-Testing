3416321e90296ef4d1f36a22ee916c5f813b1912
anthony-ng <thomsonng123@gmail.com>
2015-01-20 22:06:49 -0800

    updated skill model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,4 +3,9 @@ class Skill < ActiveRecord::Base
   has_many :users, :through => :ratings
 
   validates :name, uniqueness: true
+
+  def user_with_proficiency(rank)
+    rating_user = Rating.where(skill_id: self.id).first
+    user = User.find(rating_user.user_id)
+  end
 end
